133e2f154e0266d374409646afd019fbbb967b6b
tunahan <tunahancelebi@derinbilgi.com.tr>
2024-12-13 23:34:31 +0300

    Endpoints

--- KeycloakExample/Endpoints/UserEndpoints.cs
+++ KeycloakExample/Endpoints/UserEndpoints.cs
@@ -16,6 +16,18 @@ public static class UserEndpoints
             return Results.Ok(users);
         }).WithSummary("Tüm user'ları verir.");
 
+        group.MapGet("/get-by-username", async (string username, KeycloakService service, CancellationToken ct) =>
+        {
+            (_, var users) = await service.GetUserByUsername(username, ct);
+            return Results.Ok(users);
+        }).WithSummary("Username'e göre kullanıcı ara.");
+
+        group.MapGet("/get-by-email", async (string email, KeycloakService service, CancellationToken ct) =>
+        {
+            (_, var users) = await service.GetUserByEmail(email, ct);
+            return Results.Ok(users);
+        }).WithSummary("Email'e göre kullanıcı ara.");
+
         return routeBuilder;
     }
 }
